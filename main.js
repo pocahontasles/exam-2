dc16a5921cc7827b9cc5a1f463e45aac8eaaf1ae
Olesia <45497738+pocahontasles@users.noreply.github.com>
2019-03-03 15:03:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/*jQuery(document).ready(function () {
+    jQuery('.slider-container').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true
+    });
+});*/
+

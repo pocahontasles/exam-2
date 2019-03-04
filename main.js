4c3bce5ea4843bed0313dab6416853b37971d821
Olesia <45497738+pocahontasles@users.noreply.github.com>
2019-03-04 23:23:12 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*jQuery(document).ready(function () {
+jQuery(document).ready(function () {
     jQuery('.slider-container').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -6,5 +6,22 @@
         autoplaySpeed: 4000,
         dots: true
     });
-});*/
+});
+
+function bottomFunction() {
+    var element = document.querySelector("#contact");
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function showMap(coordinates, infoContent) {
+    var point = {lat: 40.6577799, lng: -73.9147717};
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 11, center: point});
+
+    var marker = new google.maps.Marker({
+        position: {lat: 40.6640304, lng: -73.915115},
+        map: map,
+        icon: "images/marker.png"
+    });
+}
 
